00edf9413a0a10e95254d885a94eea838346fd5a
Gabriel Trossero <gabriel.trossero@intuit.ar>
2025-08-27 23:29:45 -0300

    change

--- ProyectDemoGitHub/Controllers/WeatherForecastController.cs
+++ ProyectDemoGitHub/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ProyectDemoGitHub.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching2"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
